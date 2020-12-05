afa2bbe35cbc2c843e9e8a546c9ac1e971839c17
Gilbert <gilbert@Gilberts-MBP.fritz.box>
2020-12-05 02:34:33 +0100

    [feat] refactor and systemise w. JS generation

---
+++ docs/src/Flex.svelte
@@ -0,0 +1,44 @@
+<div class="flex flex-row w100 h80em">
+	<div class="flex b1-dashed grow w40 flex-column">
+		
+		<div class="grow b1-dotted flex flex-row">
+			
+			<div class="grow b1-dotted cross">
+				
+			</div>
+			<span class="spacer s4 cross error" />
+			<div class="grow b1-dotted cross">
+				
+			</div>
+		</div>
+		<span class="spacer s4 cross error" />
+		<div class="grow b1-dotted cross">
+			
+		</div>
+	</div>
+	<span class="spacer s6 cross error" />
+	<div class="flex flex-column b1-dashed grow">
+		<div class="grow b1-dotted cross">
+			
+		</div>
+		<span class="spacer s4 cross error" />
+		<div class="grow b1-dotted cross">
+			
+		</div>
+		<span class="spacer s4 cross error" />
+		<div class="grow b1-dotted flex flex-column">
+			<div class="cross grow" />
+			<span class="spacer s4 cross error" />
+			<div class="cross grow" />
+		</div>
+		<span class="spacer s4 cross error" />
+		<div class="grow b1-dotted cross">
+			
+		</div>
+	</div>
+	<span class="spacer s8 cross error" />
+	<div class="flex b1-dashed w20 cross">
+		
+	</div>
+
+</div>
---
+++ docs/src/Table.svelte
@@ -0,0 +1,79 @@
+<table  class="w100">
+<thead class="bright"><tr><td>class</td><td>rule</td><td>mixin</td></tr></thead><tbody>
+<tr><td>.absolute</td><td>position: absolute</td><td>+absolute</td></tr>
+<tr><td>.relative</td><td>position: relative</td><td>+relative</td></tr>
+<tr><td>.fixed</td><td>position: fixed</td><td>+fixed</td></tr>
+<tr><td>.table</td><td>display: table</td><td>+table</td></tr>
+<tr><td>.inline-block</td><td>display: inline-block</td><td>+inline-block</td></tr>
+<tr><td>.block</td><td>display: block</td><td>+block</td></tr>
+<tr><td>.flex</td><td>display: flex</td><td>+flex</td></tr>
+<tr><td>.flex-column</td><td>flex-direction: column</td><td>+flex-column</td></tr>
+<tr><td>.column</td><td>flex-direction: column</td><td>+column</td></tr>
+<tr><td>.flex-row</td><td>flex-direction: row</td><td>+flex-row</td></tr>
+<tr><td>.row</td><td>flex-direction: row</td><td>+row</td></tr>
+<tr><td>.grow</td><td>flex-grow: 1</td><td>+grow</td></tr>
+<tr><td>.no-grow</td><td>flex-grow: 0</td><td>+no-grow</td></tr>
+<tr><td>.shrink</td><td>flex-shrink: 1</td><td>+shrink</td></tr>
+<tr><td>.no-shrink</td><td>flex-shrink: 0</td><td>+no-shrink</td></tr>
+<tr><td>.no-basis</td><td>flex-basis: 0</td><td>+no-basis</td></tr>
+<tr><td>.wrap</td><td>flex-wrap: wrap</td><td>+wrap</td></tr>
+<tr><td>.no-wrap</td><td>flex-wrap: nowrap</td><td>+no-wrap</td></tr>
+<tr><td>.border-box</td><td>box-sizing: border-box</td><td>+border-box</td></tr>
+<tr><td>.text-left</td><td>text-align: left</td><td>+text-left</td></tr>
+<tr><td>.text-center</td><td>text-align: center</td><td>+text-center</td></tr>
+<tr><td>.text-right</td><td>text-align: right</td><td>+text-right</td></tr>
+<tr><td>.italic</td><td>font-style: italic</td><td>+italic</td></tr>
+<tr><td>.bold</td><td>font-weight: bold</td><td>+bold</td></tr>
+<tr><td>.bolder</td><td>font-weight: bold</td><td>+bolder</td></tr>
+<tr><td>.normal</td><td>font-weight: normal</td><td>+normal</td></tr>
+<tr><td>.light</td><td>font-weight: light</td><td>+light</td></tr>
+<tr><td>.lighter</td><td>font-weight: lighter</td><td>+lighter</td></tr>
+<tr><td>.visible</td><td>opacity: 1</td><td>+visible</td></tr>
+<tr><td>.invisible</td><td>opacity: 0</td><td>+invisible</td></tr>
+<tr><td>.no-bg</td><td>background: transparent</td><td>+no-bg</td></tr>
+<tr><td>.align-items-[center|start|end|flex-start|flex-end|stretch]</td><td>align-items: [option]</td>+align-items-[center|start|end|flex-start|flex-end|stretch]<td></tr>
+<tr><td>.align-self-[center|start|end|flex-start|flex-end|stretch]</td><td>align-self: [option]</td>+align-self-[center|start|end|flex-start|flex-end|stretch]<td></tr>
+<tr><td>.align-content-[center|start|end|flex-start|flex-end|stretch]</td><td>align-content: [option]</td>+align-content-[center|start|end|flex-start|flex-end|stretch]<td></tr>
+<tr><td>.justify-items-[between|evenly|around|center|start|end|stretch|flex-start|flex-end]</td><td>justify-items: [option]</td>+justify-items-[between|evenly|around|center|start|end|stretch|flex-start|flex-end]<td></tr>
+<tr><td>.justify-self-[between|evenly|around|center|start|end|stretch|flex-start|flex-end]</td><td>justify-self: [option]</td>+justify-self-[between|evenly|around|center|start|end|stretch|flex-start|flex-end]<td></tr>
+<tr><td>.justify-content-[between|evenly|around|center|start|end|stretch|flex-start|flex-end]</td><td>justify-content: [option]</td>+justify-content-[between|evenly|around|center|start|end|stretch|flex-start|flex-end]<td></tr>
+<tr><td>.twelve-[0-80]</td><td>(.col) padding: 0 [spacing] 0 [spacing]</td><td>+twelve( $spacing: 0em )</td></tr>
+<tr><td>.p[0-80]</td><td>padding: [0-80]em</td></tr>
+<tr><td>.pt[0-80]</td><td>padding-top: [0-80]em</td></tr>
+<tr><td>.pr[0-80]</td><td>padding-right: [0-80]em</td></tr>
+<tr><td>.pb[0-80]</td><td>padding-bottom: [0-80]em</td></tr>
+<tr><td>.pl[0-80]</td><td>padding-left: [0-80]em</td></tr>
+<tr><td>.ptb[0-80]<br />.pbt[0-80]</td><td>padding-top: [0-80]em<br />padding-bottom: [0-80]em</td></tr>
+<tr><td>.plr[0-80]<br />.prl[0-80]</td><td>padding-left: [0-80]em<br />padding-right: [0-80]em</td></tr>
+<tr><td>.s[0-80]</td><td>flex-basis: [0-80]em</td></tr>
+<tr><td>.m[0-80]</td><td>margin: [0-80]em</td></tr>
+<tr><td>.mt[0-80]</td><td>margin-top: [0-80]em</td></tr>
+<tr><td>.mr[0-80]</td><td>margin-right: [0-80]em</td></tr>
+<tr><td>.mb[0-80]</td><td>margin-bottom: [0-80]em</td></tr>
+<tr><td>.ml[0-80]</td><td>margin-left: [0-80]em</td></tr>
+<tr><td>.mtb[0-80]<br />.mbt[0-80]</td><td>margin-top: [0-80]em<br />margin-bottom: [0-80]em</td></tr>
+<tr><td>.mlr[0-80]<br />.mrl[0-80]</td><td>margin-left: [0-80]em<br />margin-right: [0-80]em</td></tr>
+<tr><td>.s[0-80]</td><td>flex-basis: [0-80]em</td></tr>	
+<tr><td>.b[0-8]-[solid|dashed|dotted]</td><td>border-width: [0-8]px<br />border-style: [solid|dashed|dotted]</td></tr>
+<tr><td>.btb[0-8]-[solid|dashed|dotted]<br />.bbt[0-8]-[solid|dashed|dotted]</td><td>border-top-width: [0-8]px<br />border-bottom-width: [0-8]px<br />border-style: [solid|dashed|dotted]</td></tr>
+<tr><td>.blr[0-8]-[solid|dashed|dotted]<br />.brl[0-8]-[solid|dashed|dotted]</td><td>border-left-width: [0-8]px<br />border-right-width: [0-8]px<br />border-style: [solid|dashed|dotted]</td></tr>
+<tr><td>.br[0-8]-[solid|dashed|dotted]</td><td>border-right-width: [0-8]px<br />border-style: [solid|dashed|dotted]</td></tr>
+<tr><td>.bt[0-8]-[solid|dashed|dotted]</td><td>border-top-width: [0-8]px<br />border-style: [solid|dashed|dotted]</td></tr>
+<tr><td>.bb[0-8]-[solid|dashed|dotted]</td><td>border-bottom-width: [0-8]px<br />border-style: [solid|dashed|dotted]</td></tr>
+<tr><td>.bl[0-8]-[solid|dashed|dotted]</td><td>border-left-width: [0-8]px<br />border-style: [solid|dashed|dotted]</td></tr>
+		
+<tr><td>.t[0-100]</td><td>top: [0-100]%</td></tr>
+<tr><td>.t[0-100]em</td><td>top: [0-100]em</td></tr>
+<tr><td>.l[0-100]</td><td>left: [0-100]%</td></tr>
+<tr><td>.l[0-100]em</td><td>left: [0-100]em</td></tr>
+<tr><td>.b[0-100]</td><td>bottom: [0-100]%</td></tr>
+<tr><td>.b[0-100]em</td><td>bottom: [0-100]em</td></tr>
+<tr><td>.r[0-100]</td><td>right: [0-100]%</td></tr>
+<tr><td>.r[0-100]em</td><td>right: [0-100]em</td></tr>
+<tr><td>.w[0-100]</td><td>width: [0-100]%</td></tr>
+<tr><td>.w[0-100]em</td><td>width: [0-100]em</td></tr>
+<tr><td>.h[0-100]</td><td>height: [0-100]%</td></tr>
+<tr><td>.h[0-100]em</td><td>height: [0-100]em</td></tr>
+<tr><td>.o[0-100]</td><td>transform-origin: [0-100]% [0-100]%</td></tr>
+
+</tbody></table>
