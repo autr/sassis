2c508e8d6bb7b07513eeb1fa03811ffcfefe4be0
GSinnott <g@sinnott.cc>
2020-12-11 18:10:35 +0100

    iterations

--- docs/src/Table.svelte
+++ docs/src/Table.svelte
@@ -1,43 +1,5 @@
 <table  class="w100">
 <thead class="bright"><tr><td>class</td><td>rule</td><td>mixin</td></tr></thead><tbody>
-<tr><td>.absolute</td><td>position: absolute</td><td>+absolute</td></tr>
-<tr><td>.relative</td><td>position: relative</td><td>+relative</td></tr>
-<tr><td>.fixed</td><td>position: fixed</td><td>+fixed</td></tr>
-<tr><td>.table</td><td>display: table</td><td>+table</td></tr>
-<tr><td>.inline-block</td><td>display: inline-block</td><td>+inline-block</td></tr>
-<tr><td>.block</td><td>display: block</td><td>+block</td></tr>
-<tr><td>.flex</td><td>display: flex</td><td>+flex</td></tr>
-<tr><td>.flex-column</td><td>flex-direction: column</td><td>+flex-column</td></tr>
-<tr><td>.column</td><td>flex-direction: column</td><td>+column</td></tr>
-<tr><td>.flex-row</td><td>flex-direction: row</td><td>+flex-row</td></tr>
-<tr><td>.row</td><td>flex-direction: row</td><td>+row</td></tr>
-<tr><td>.grow</td><td>flex-grow: 1</td><td>+grow</td></tr>
-<tr><td>.no-grow</td><td>flex-grow: 0</td><td>+no-grow</td></tr>
-<tr><td>.shrink</td><td>flex-shrink: 1</td><td>+shrink</td></tr>
-<tr><td>.no-shrink</td><td>flex-shrink: 0</td><td>+no-shrink</td></tr>
-<tr><td>.no-basis</td><td>flex-basis: 0</td><td>+no-basis</td></tr>
-<tr><td>.wrap</td><td>flex-wrap: wrap</td><td>+wrap</td></tr>
-<tr><td>.no-wrap</td><td>flex-wrap: nowrap</td><td>+no-wrap</td></tr>
-<tr><td>.border-box</td><td>box-sizing: border-box</td><td>+border-box</td></tr>
-<tr><td>.text-left</td><td>text-align: left</td><td>+text-left</td></tr>
-<tr><td>.text-center</td><td>text-align: center</td><td>+text-center</td></tr>
-<tr><td>.text-right</td><td>text-align: right</td><td>+text-right</td></tr>
-<tr><td>.italic</td><td>font-style: italic</td><td>+italic</td></tr>
-<tr><td>.bold</td><td>font-weight: bold</td><td>+bold</td></tr>
-<tr><td>.bolder</td><td>font-weight: bold</td><td>+bolder</td></tr>
-<tr><td>.normal</td><td>font-weight: normal</td><td>+normal</td></tr>
-<tr><td>.light</td><td>font-weight: light</td><td>+light</td></tr>
-<tr><td>.lighter</td><td>font-weight: lighter</td><td>+lighter</td></tr>
-<tr><td>.visible</td><td>opacity: 1</td><td>+visible</td></tr>
-<tr><td>.invisible</td><td>opacity: 0</td><td>+invisible</td></tr>
-<tr><td>.no-bg</td><td>background: transparent</td><td>+no-bg</td></tr>
-<tr><td>.pointer</td><td>cursor: pointer</td><td>+pointer</td></tr>
-<tr><td>.align-items-[center|start|end|flex-start|flex-end|stretch]</td><td>align-items: [option]</td>+align-items-[center|start|end|flex-start|flex-end|stretch]<td></tr>
-<tr><td>.align-self-[center|start|end|flex-start|flex-end|stretch]</td><td>align-self: [option]</td>+align-self-[center|start|end|flex-start|flex-end|stretch]<td></tr>
-<tr><td>.align-content-[center|start|end|flex-start|flex-end|stretch]</td><td>align-content: [option]</td>+align-content-[center|start|end|flex-start|flex-end|stretch]<td></tr>
-<tr><td>.justify-items-[between|evenly|around|center|start|end|stretch|flex-start|flex-end]</td><td>justify-items: [option]</td>+justify-items-[between|evenly|around|center|start|end|stretch|flex-start|flex-end]<td></tr>
-<tr><td>.justify-self-[between|evenly|around|center|start|end|stretch|flex-start|flex-end]</td><td>justify-self: [option]</td>+justify-self-[between|evenly|around|center|start|end|stretch|flex-start|flex-end]<td></tr>
-<tr><td>.justify-content-[between|evenly|around|center|start|end|stretch|flex-start|flex-end]</td><td>justify-content: [option]</td>+justify-content-[between|evenly|around|center|start|end|stretch|flex-start|flex-end]<td></tr>
 <tr><td>.twelve-[0-80]</td><td>(.col) padding: 0 [spacing] 0 [spacing]</td><td>+twelve( $spacing: 0em )</td></tr>
 <tr><td>.p[0-80]</td><td>padding: [0-80]em</td></tr>
 <tr><td>.pt[0-80]</td><td>padding-top: [0-80]em</td></tr>
@@ -80,6 +42,27 @@
 
 
 
+<tr><td>.s[0-80]</td><td>flex-basis: [0-80]em</td></tr>
+<tr><td>.[0-80]</td><td>: [0-80]em</td></tr>
+<tr><td>.t[0-80]</td><td>top: [0-80]em</td></tr>
+<tr><td>.r[0-80]</td><td>right: [0-80]em</td></tr>
+<tr><td>.b[0-80]</td><td>bottom: [0-80]em</td></tr>
+<tr><td>.l[0-80]</td><td>left: [0-80]em</td></tr>
+<tr><td>.tb[0-80]<br />.bt[0-80]</td><td>top: [0-80]em<br />bottom: [0-80]em</td></tr>
+<tr><td>.lr[0-80]<br />.rl[0-80]</td><td>left: [0-80]em<br />right: [0-80]em</td></tr>
+
+<!-- child elements -->
+
+<tr><td>.c[0-80]</td><td>: [0-80]em</td></tr>
+<tr><td>.ct[0-80]</td><td>top: [0-80]em</td></tr>
+<tr><td>.cr[0-80]</td><td>right: [0-80]em</td></tr>
+<tr><td>.cb[0-80]</td><td>bottom: [0-80]em</td></tr>
+<tr><td>.cl[0-80]</td><td>left: [0-80]em</td></tr>
+<tr><td>.ctb[0-80]<br />.bt[0-80]</td><td>top: [0-80]em<br />bottom: [0-80]em</td></tr>
+<tr><td>.clr[0-80]<br />.rl[0-80]</td><td>left: [0-80]em<br />right: [0-80]em</td></tr>
+
+
+
 <tr><td>.s[0-80]</td><td>flex-basis: [0-80]em</td></tr>	
 <tr><td>.b[0-8]-[solid|dashed|dotted]</td><td>border-width: [0-8]px<br />border-style: [solid|dashed|dotted]</td></tr>
 <tr><td>.btb[0-8]-[solid|dashed|dotted]<br />.bbt[0-8]-[solid|dashed|dotted]</td><td>border-top-width: [0-8]px<br />border-bottom-width: [0-8]px<br />border-style: [solid|dashed|dotted]</td></tr>
@@ -88,26 +71,32 @@
 <tr><td>.bt[0-8]-[solid|dashed|dotted]</td><td>border-top-width: [0-8]px<br />border-style: [solid|dashed|dotted]</td></tr>
 <tr><td>.bb[0-8]-[solid|dashed|dotted]</td><td>border-bottom-width: [0-8]px<br />border-style: [solid|dashed|dotted]</td></tr>
 <tr><td>.bl[0-8]-[solid|dashed|dotted]</td><td>border-left-width: [0-8]px<br />border-style: [solid|dashed|dotted]</td></tr>
+<tr><td>.radius[0-8]</td><td>border-radius: [0-8]px</td></tr>
 		
-<tr><td>.t[0-100]</td><td>top: [0-100]%</td></tr>
+<tr><td>.t[0-100]pc</td><td>top: [0-100]%</td></tr>
 <tr><td>.t[0-100]em</td><td>top: [0-100]em</td></tr>
-<tr><td>.l[0-100]</td><td>left: [0-100]%</td></tr>
+<tr><td>.l[0-100]pc</td><td>left: [0-100]%</td></tr>
 <tr><td>.l[0-100]em</td><td>left: [0-100]em</td></tr>
-<tr><td>.b[0-100]</td><td>bottom: [0-100]%</td></tr>
+<tr><td>.b[0-100]pc</td><td>bottom: [0-100]%</td></tr>
 <tr><td>.b[0-100]em</td><td>bottom: [0-100]em</td></tr>
-<tr><td>.r[0-100]</td><td>right: [0-100]%</td></tr>
+<tr><td>.r[0-100]pc</td><td>right: [0-100]%</td></tr>
 <tr><td>.r[0-100]em</td><td>right: [0-100]em</td></tr>
-<tr><td>.w[0-100]</td><td>width: [0-100]%</td></tr>
+<tr><td>.w[0-100]pc</td><td>width: [0-100]%</td></tr>
 <tr><td>.w[0-100]em</td><td>width: [0-100]em</td></tr>
-<tr><td>.h[0-100]</td><td>height: [0-100]%</td></tr>
+<tr><td>.h[0-100]pc</td><td>height: [0-100]%</td></tr>
 <tr><td>.h[0-100]em</td><td>height: [0-100]em</td></tr>
-<tr><td>.o[0-100]</td><td>transform-origin: [0-100]% [0-100]%</td></tr>
-<tr><td>.max-width[0-100]</td><td>max-width: [0-100]%</td></tr>
+<tr><td>.max-width[0-100]pc</td><td>max-width: [0-100]%</td></tr>
 <tr><td>.max-width[0-100]em</td><td>max-width: [0-100]em</td></tr>
-<tr><td>.max-height[0-100]</td><td>max-height: [0-100]%</td></tr>
+<tr><td>.max-height[0-100]pc</td><td>max-height: [0-100]%</td></tr>
 <tr><td>.max-height[0-100]em</td><td>max-height: [0-100]em</td></tr>
+<tr><td>.min-width[0-100]pc</td><td>min-width: [0-100]%</td></tr>
+<tr><td>.min-width[0-100]em</td><td>min-width: [0-100]em</td></tr>
+<tr><td>.min-height[0-100]pc</td><td>min-height: [0-100]%</td></tr>
+<tr><td>.min-height[0-100]em</td><td>min-height: [0-100]em</td></tr>
 <tr><td>.vw[0-100]</td><td>width: [0-100]vw</td></tr>
 <tr><td>.vh[0-100]</td><td>height: [0-100]vh</td></tr>
+<tr><td>.basis[0-100]</td><td>flex-basis: [0-100]%</td></tr>
+<tr><td>.basis[0-100]em</td><td>flex-basis: [0-100]em</td></tr>
 
 
 </tbody></table>
--- src/shorthand.js
+++ src/shorthand.js
@@ -16,86 +16,154 @@ classes += '\n\n\t// ---------- 001 basic ----------\n\n'
 // --------------------------------------------------------------------------------
 
 
-const aliases = {
+const aliases = [
 
-	'absolute': 'position: absolute',
-	'relative': 'position: relative',
-	'fixed': 'position: fixed',
-	'table': 'display: table',
-	'inline-block': 'display: inline-block',
-	'block': 'display: block',
-	'flex': 'display: flex',
-	'flex-column': 'flex-direction: column',
-	'column': 'flex-direction: column',
-	'flex-row': 'flex-direction: row',
-	'row': 'flex-direction: row',
-	'grow': 'flex-grow: 1',
-	'no-grow': 'flex-grow: 0',
-	'shrink': 'flex-shrink: 1',
-	'no-shrink': 'flex-shrink: 0',
-	'no-basis': 'flex-basis: 0',
-	'wrap': 'flex-wrap: wrap',
-	'no-wrap': 'flex-wrap: nowrap',
-	'border-box': 'box-sizing: border-box',
-	'wrap': 'flex-wrap: wrap',
-	'no-wrap': 'flex-wrap: nowrap',
-	'text-left': 'text-align: left',
-	'text-center': 'text-align: center',
-	'text-right': 'text-align: right',
-	'italic': 'font-style: italic',
-	'bold': 'font-weight: bold',
-	'bolder': 'font-weight: bold',
-	'normal': 'font-weight: normal',
-	'light': 'font-weight: light',
-	'lighter': 'font-weight: lighter',
-	'visible': 'opacity: 1',
-	'invisible': 'opacity: 0',
-	'no-bg': 'background: transparent',
-	'pointer': 'cursor: pointer'
-}
+	[ ['absolute', 'abs'], 'position: absolute' ],
+	[ ['relative', 'rel'], 'position: relative' ],
+	[ ['fixed', 'fix'], 'position: fixed' ],
+	[ ['sticky', 'stick'], 'position: sticky' ],
+	[ ['table'], 'display: table' ],
+	[ ['inline-block', 'iblock'], 'display: inline-block' ],
+	[ ['block'], 'display: block' ],
+	[ ['flex', 'fx'], 'display: flex' ],
+	[ ['flex-column', 'column', 'fx-col'], 'flex-direction: column' ],
+	[ ['flex-row', 'row', 'fx-row'], 'flex-direction: row' ],
+	[ ['grow'], 'flex-grow: 1' ],
+	[ ['no-grow'], 'flex-grow: 0' ],
+	[ ['shrink'], 'flex-shrink: 1' ],
+	[ ['no-shrink'], 'flex-shrink: 0' ],
+	[ ['no-basis'], 'flex-basis: 0' ],
+	[ ['wrap'], 'flex-wrap: wrap' ],
+	[ ['no-wrap'], 'flex-wrap: nowrap' ],
+	[ ['border-box'], 'box-sizing: border-box' ],
+	[ ['wrap'], 'flex-wrap: wrap' ],
+	[ ['no-wrap'], 'flex-wrap: nowrap' ],
+	[ ['text-left'], 'text-align: left' ],
+	[ ['text-center'], 'text-align: center' ],
+	[ ['text-right'], 'text-align: right' ],
+	[ ['italic'], 'font-style: italic' ],
+	[ ['bold'], 'font-weight: bold' ],
+	[ ['bolder'], 'font-weight: bold' ],
+	[ ['normal'], 'font-weight: normal' ],
+	[ ['light'], 'font-weight: light' ],
+	[ ['lighter'], 'font-weight: lighter' ],
+	[ ['visible'], 'opacity: 1' ],
+	[ ['invisible'], 'opacity: 0' ],
+	[ ['no-bg'], 'background: transparent' ],
+	[ ['pointer'], 'cursor: pointer' ],
+	[ ['overflow-hidden', 'hidden'], 'overflow: hidden' ],
+	[ ['overflow-auto'], 'overflow: auto' ]
+]
 
 
 
-Object.keys( aliases ).forEach( name => {
-	const rule = aliases[name]
-	mixins += `\n=${name}\n\t${rule}\n`
-	classes += `\n\t.${name}\n\t\t${rule}\n`
-	table += `\n<tr><td>.${name}</td><td>${rule}</td><td>+${name}</td></tr>`
+aliases.forEach( pair => {
+	const names = pair[0]
+	const rule = pair[1]
+	mixins += `\n=${names.join(`\n\t${rule}\n=`)}\n\t${rule}\n`
+	classes += `\n\t.${names.join(', .')}\n\t\t${rule}\n`
 })
 
 
-const two = ['align', 'justify']
-two.forEach( main => {
-	['items', 'self', 'content'].forEach( type => {
-		const prop = `${main}-${type}`
-		const alles = (main == 'align') ? 
-			[
-				['center', 'center'],
-				['start', 'start'],
-				['end', 'end'],
-				['flex-start', 'flex-start'],
-				['flex-end', 'flex-end'], 
-				['stretch', 'stretch']]
-		: [['between', 'space-between'],
-		['evenly', 'space-evenly'],
-		['around', 'space-around'],
-		['center', 'center'],
-		['start', 'start'],
-		['end', 'end'], 
-		['stretch', 'stretch'],
-		['flex-start', 'flex-start'],
-		['flex-end', 'flex-end']];
-		const options = alles.map( a => a[0] ).join('|')
-		alles.forEach( pair => {
-			const short = pair[0]
-			const className = `${prop}-${short}`
-			const val = pair[1]
-			mixins += `\n=${className}\n\t${prop}: ${val}\n`
-			classes += `\n\t.${className}\n\t\t${prop}: ${val}\n`
+const alignments = [
+	[ // rule
+		[ // 0 = names
+			[ 'align-self', 'a-s' ], 
+			[ 'align-items', 'a-i' ], 
+			[ 'justify-self', 'j-s' ], 
+			[ 'justify-items', 'j-i' ]
+		], 
+		[ // 1 = props
+			[ 'center', 'c' ], 
+			[ 'end', 'e' ], 
+			[ 'flex-end', 'fe' ], 
+			[ 'start', 's' ], 
+			[ 'flex-start', 'fs' ], 
+			[ 'stretch', 'str', 'st' ]
+		]
+	],
+	[
+		[ // 0 = names
+			[ 'align-content', 'a-c' ], 
+			[ 'justify-content', 'j-c' ], 
+		], 
+		[ // 1 = props
+			[ 'space-between', 'between', 'b' ],
+			[ 'space-evenly', 'evenly', 'e' ],
+			[ 'space-around', 'around', 'a' ],
+			[ 'left', 'l' ],
+			[ 'right', 'r' ],
+			[ 'center', 'c' ], 
+			[ 'end', 'e' ], 
+			[ 'flex-end', 'fe' ], 
+			[ 'start', 's' ], 
+			[ 'flex-start', 'fs' ], 
+			[ 'stretch', 'str', 'st' ]
+		]
+	]
+]
+
+alignments.forEach( a => {
+	const nameMega = a[0]
+	const values = a[1]
+
+	values.forEach( classList => {
+		nameMega.forEach( nameList => {
+
+			let rule = []
+			nameList.forEach( name => {			
+
+
+					classList.forEach( className => {
+						rule.push( name + '-' + className )
+					})
+
+
+			})
+			rule = '.' + rule.join( ', .' )
+			const validProp = nameList[0]
+			const validValue = classList[0]
+			rule += '\n\t' + validProp + ': ' + validValue
+			console.log('------', rule)
+			classes += `\n${rule}\n`
+
 		})
-		table += `\n<tr><td>.${prop}-[${options}]</td><td>${prop}: [option]</td>+${prop}-[${options}]<td></tr>`
 	})
 })
+
+
+// const two = ['align', 'justify' ]
+// two.forEach( main => {
+// 	['items', 'self', 'content'].forEach( type => {
+// 		const prop = `${main}-${type}`
+// 		const alles = (main == 'align') ? 
+// 			[
+// 				['center', 'center'],
+// 				['start', 'start'],
+// 				['end', 'end'],
+// 				['flex-start', 'flex-start'],
+// 				['flex-end', 'flex-end'], 
+// 				['stretch', 'stretch']]
+// 		: [['between', 'space-between'],
+// 		['evenly', 'space-evenly'],
+// 		['around', 'space-around'],
+// 		['center', 'center'],
+// 		['start', 'start'],
+// 		['end', 'end'], 
+// 		['stretch', 'stretch'],
+// 		['flex-start', 'flex-start'],
+// 		['flex-end', 'flex-end']];
+// 		const options = alles.map( a => a[0] ).join('|')
+// 		alles.forEach( pair => {
+// 			const short = pair[0]
+// 			const className = `${prop}-${short}`
+// 			const val = pair[1]
+// 			mixins += `\n=${className}\n\t${prop}: ${val}\n`
+// 			classes += `\n\t.${className}\n\t\t${prop}: ${val}\n`
+// 		})
+// 		table += `\n<tr><td>.${prop}-[${options}]</td><td>${prop}: [option]</td>+${prop}-[${options}]<td></tr>`
+// 	})
+// })
 
 
 // --------------------------------------------------------------------------------
@@ -121,52 +189,63 @@ for ( let i = 0; i <= 80; i += 2 ) {
 <tr><td>.twelve-[0-80]</td><td>(.col) padding: 0 [spacing] 0 [spacing]</td><td>+twelve( $spacing: 0em )</td></tr>`;
 	}
 
-	[ ['p', 'padding'], ['m', 'margin'] ].forEach( two => {
+	[ ['p', 'padding'], ['m', 'margin'], ['', ''] ].forEach( two => {
 		const short = two[0]
 		const prop = two[1]
-		const text = `
-	.${short}${num}
-		${prop}: ${val}em
-	.${short}t${num}
-		${prop}-top: ${val}em
-	.${short}r${num}
-		${prop}-right: ${val}em
-	.${short}b${num}
-		${prop}-bottom: ${val}em
-	.${short}l${num}
-		${prop}-left: ${val}em
-	.${short}tb${num}, .${short}bt${num}
-		${prop}-top: ${val}em
-		${prop}-bottom: ${val}em
-	.${short}lr${num}, .${short}rl${num}
-		${prop}-left: ${val}em
-		${prop}-right: ${val}em
+		const propDash = (short != '') ? prop + '-' : ''
+		let text = ''
+		text += short != '' ? `
 
-	// child elements
+	// main ${short} elements
+
+	.${short}${num}
+		${prop}: ${val}em ` : ''
+
+		text += `
+	.${short}t${num}
+		${propDash}top: ${val}em
+	.${short}r${num}
+		${propDash}right: ${val}em
+	.${short}b${num}
+		${propDash}bottom: ${val}em
+	.${short}l${num}
+		${propDash}left: ${val}em
+	.${short}tb${num}, .${short}bt${num}
+		${propDash}top: ${val}em
+		${propDash}bottom: ${val}em
+	.${short}lr${num}, .${short}rl${num}
+		${propDash}left: ${val}em
+		${propDash}right: ${val}em`
+
+		text += short != '' ? `
+
+	// child ${short} elements
 
 	.c${short}${num}
 		> *
-			${prop}: ${val}em
+			${prop}: ${val}em` : ''
+
+		text += `
 	.c${short}t${num}
 		> *
-			${prop}-top: ${val}em
+			${propDash}top: ${val}em
 	.c${short}r${num}
 		> *
-			${prop}-right: ${val}em
+			${propDash}right: ${val}em
 	.c${short}b${num}
 		> *
-			${prop}-bottom: ${val}em
+			${propDash}bottom: ${val}em
 	.c${short}l${num}
 		> *
-			${prop}-left: ${val}em
+			${propDash}left: ${val}em
 	.c${short}tb${num}, .c${short}bt${num}
 		> *
-			${prop}-top: ${val}em
-			${prop}-bottom: ${val}em
+			${propDash}top: ${val}em
+			${propDash}bottom: ${val}em
 	.c${short}lr${num}, .c${short}rl${num}
 		> *
-			${prop}-left: ${val}em
-			${prop}-right: ${val}em
+			${propDash}left: ${val}em
+			${propDash}right: ${val}em
 		`;
 		classes += text + '\n'
 
@@ -174,85 +253,92 @@ for ( let i = 0; i <= 80; i += 2 ) {
 
 			table += `
 <tr><td>.${short}[0-80]</td><td>${prop}: [0-80]em</td></tr>
-<tr><td>.${short}t[0-80]</td><td>${prop}-top: [0-80]em</td></tr>
-<tr><td>.${short}r[0-80]</td><td>${prop}-right: [0-80]em</td></tr>
-<tr><td>.${short}b[0-80]</td><td>${prop}-bottom: [0-80]em</td></tr>
-<tr><td>.${short}l[0-80]</td><td>${prop}-left: [0-80]em</td></tr>
-<tr><td>.${short}tb[0-80]<br />.${short}bt[0-80]</td><td>${prop}-top: [0-80]em<br />${prop}-bottom: [0-80]em</td></tr>
-<tr><td>.${short}lr[0-80]<br />.${short}rl[0-80]</td><td>${prop}-left: [0-80]em<br />${prop}-right: [0-80]em</td></tr>
+<tr><td>.${short}t[0-80]</td><td>${propDash}top: [0-80]em</td></tr>
+<tr><td>.${short}r[0-80]</td><td>${propDash}right: [0-80]em</td></tr>
+<tr><td>.${short}b[0-80]</td><td>${propDash}bottom: [0-80]em</td></tr>
+<tr><td>.${short}l[0-80]</td><td>${propDash}left: [0-80]em</td></tr>
+<tr><td>.${short}tb[0-80]<br />.${short}bt[0-80]</td><td>${propDash}top: [0-80]em<br />${propDash}bottom: [0-80]em</td></tr>
+<tr><td>.${short}lr[0-80]<br />.${short}rl[0-80]</td><td>${propDash}left: [0-80]em<br />${propDash}right: [0-80]em</td></tr>
 
 <!-- child elements -->
 
 <tr><td>.c${short}[0-80]</td><td>${prop}: [0-80]em</td></tr>
-<tr><td>.c${short}t[0-80]</td><td>${prop}-top: [0-80]em</td></tr>
-<tr><td>.c${short}r[0-80]</td><td>${prop}-right: [0-80]em</td></tr>
-<tr><td>.c${short}b[0-80]</td><td>${prop}-bottom: [0-80]em</td></tr>
-<tr><td>.c${short}l[0-80]</td><td>${prop}-left: [0-80]em</td></tr>
-<tr><td>.c${short}tb[0-80]<br />.${short}bt[0-80]</td><td>${prop}-top: [0-80]em<br />${prop}-bottom: [0-80]em</td></tr>
-<tr><td>.c${short}lr[0-80]<br />.${short}rl[0-80]</td><td>${prop}-left: [0-80]em<br />${prop}-right: [0-80]em</td></tr>
+<tr><td>.c${short}t[0-80]</td><td>${propDash}top: [0-80]em</td></tr>
+<tr><td>.c${short}r[0-80]</td><td>${propDash}right: [0-80]em</td></tr>
+<tr><td>.c${short}b[0-80]</td><td>${propDash}bottom: [0-80]em</td></tr>
+<tr><td>.c${short}l[0-80]</td><td>${propDash}left: [0-80]em</td></tr>
+<tr><td>.c${short}tb[0-80]<br />.${short}bt[0-80]</td><td>${propDash}top: [0-80]em<br />${propDash}bottom: [0-80]em</td></tr>
+<tr><td>.c${short}lr[0-80]<br />.${short}rl[0-80]</td><td>${propDash}left: [0-80]em<br />${propDash}right: [0-80]em</td></tr>
 
 
 `
 
 			table += '\n<tr><td>.s[0-80]</td><td>flex-basis: [0-80]em</td></tr>'
-
+			let a = ( short != '' ) ? `\n=${short}( $amount )\n\t${prop}: $amount` : ''
+			let b = ( short != '' ) ? `\n=c${short}( $amount )\n\t>*\n\t\t${prop}: $amount` : ''
 			const mixin = `
-=${short}( $amount )
-	${prop}: $amount
+
+// main ${short} elements
+
+${a}
+
+// ...
+
 =${short}t( $amount )
-	${prop}-top: $amount
+	${propDash}top: $amount
 =${short}r( $amount )
-	${prop}-right: $amount
+	${propDash}right: $amount
 =${short}b( $amount )
-	${prop}-bottom: $amount
+	${propDash}bottom: $amount
 =${short}l( $amount )
-	${prop}-left: $amount
+	${propDash}left: $amount
 =${short}tb( $amount )
-	${prop}-top: $amount
-	${prop}-bottom: $amount
+	${propDash}top: $amount
+	${propDash}bottom: $amount
 =${short}bt( $amount )
-	${prop}-top: $amount
-	${prop}-bottom: $amount
+	${propDash}top: $amount
+	${propDash}bottom: $amount
 =${short}lr( $amount )
-	${prop}-left: $amount
-	${prop}-right: $amount
+	${propDash}left: $amount
+	${propDash}right: $amount
 =${short}rl( $amount )
-	${prop}-left: $amount
-	${prop}-right: $amount
+	${propDash}left: $amount
+	${propDash}right: $amount
 
-// child elements
+// child ${short} elements
 
-=c${short}( $amount )
-	> *
-		${prop}: $amount
+${b}
+
+// ...
+
 =c${short}t( $amount )
 	> *
-		${prop}-top: $amount
+		${propDash}top: $amount
 =c${short}r( $amount )
 	> *
-		${prop}-right: $amount
+		${propDash}right: $amount
 =c${short}b( $amount )
 	> *
-		${prop}-bottom: $amount
+		${propDash}bottom: $amount
 =c${short}l( $amount )
 	> *
-		${prop}-left: $amount
+		${propDash}left: $amount
 =c${short}tb( $amount )
 	> *
-		${prop}-top: $amount
-	${prop}-bottom: $amount
+		${propDash}top: $amount
+		${propDash}bottom: $amount
 =c${short}bt( $amount )
 	> *
-		${prop}-top: $amount
-	${prop}-bottom: $amount
+		${propDash}top: $amount
+		${propDash}bottom: $amount
 =c${short}lr( $amount )
 	> *
-		${prop}-left: $amount
-	${prop}-right: $amount
+		${propDash}left: $amount
+		${propDash}right: $amount
 =c${short}rl( $amount )
 	> *
-		${prop}-left: $amount
-	${prop}-right: $amount
+		${propDash}left: $amount
+		${prop}-right: $amount
 			`;
 			mixins += mixin + '\n'
 		}
@@ -318,6 +404,9 @@ for ( let i = 0; i <= 8; i ++ ) {
 
 	})
 
+
+		classes += `\n\t.radius${i}\n\t\tborder-radius: ${i}px\n`
+
 	if (i == 0) {
 		table += `	
 <tr><td>.b[0-8]-[solid|dashed|dotted]</td><td>border-width: [0-8]px<br />border-style: [solid|dashed|dotted]</td></tr>
@@ -327,6 +416,7 @@ for ( let i = 0; i <= 8; i ++ ) {
 <tr><td>.bt[0-8]-[solid|dashed|dotted]</td><td>border-top-width: [0-8]px<br />border-style: [solid|dashed|dotted]</td></tr>
 <tr><td>.bb[0-8]-[solid|dashed|dotted]</td><td>border-bottom-width: [0-8]px<br />border-style: [solid|dashed|dotted]</td></tr>
 <tr><td>.bl[0-8]-[solid|dashed|dotted]</td><td>border-left-width: [0-8]px<br />border-style: [solid|dashed|dotted]</td></tr>
+<tr><td>.radius[0-8]</td><td>border-radius: [0-8]px</td></tr>
 		`
 	}
 }
@@ -341,26 +431,39 @@ classes += '\n\n\t// ---------- 004 top left bottom right width height origin --
 classes += '\n\n\t// top left right bottom width height\n\n'
 
 for ( let i = 0; i <= 100; i++ ) {
-	const tlrbwh = {'t': 'top','l': 'left','b': 'bottom','r': 'right','w': 'width','h': 'height', 'o': 'transform-origin', 'max-width': 'max-width', 'max-height': 'max-height'};
+	const tlrbwh = {
+		't': 'top',
+		'l': 'left',
+		'b': 'bottom',
+		'r': 'right',
+		'w': 'width',
+		'h': 'height',
+		'max-width': 'max-width',
+		'max-height': 'max-height',
+		'min-width': 'min-width',
+		'min-height': 'min-height',
+		'z': 'z-index'
+	}
 
 	Object.keys( tlrbwh ).forEach( letter => {
 
 		const property = tlrbwh[ letter ]
 
-
-
-		if (letter != 'o') {
-			classes += `\t.${letter}${i}em\n\t\t${property}: ${i}em\n`
-			classes += `\t.${letter}${i}\n\t\t${property}: ${i}%\n`
-			if (i == 0) table += `\n<tr><td>.${letter}[0-100]</td><td>${property}: [0-100]%</td></tr>\n<tr><td>.${letter}[0-100]em</td><td>${property}: [0-100]em</td></tr>`
+		if (letter == 'z') {
+			classes += `\t.${letter}${i}\n\t\t${property}: ${i}\n`
+		} else if (letter == 'o' ) {
+			classes += `\t.${letter}${i}pc\n\t\t${property}: ${i}% ${i}%\n`
+			if (i == 0) table += `\n<tr><td>.${letter}[0-100]pc</td><td>${property}: [0-100]% [0-100]%</td></tr>`
 		} else {
-			classes += `\t.${letter}${i}\n\t\t${property}: ${i}% ${i}%\n`
-			if (i == 0) table += `\n<tr><td>.${letter}[0-100]</td><td>${property}: [0-100]% [0-100]%</td></tr>`
+			classes += `\t.${letter}${i}em\n\t\t${property}: ${i}em\n`
+			classes += `\t.${letter}${i}pc\n\t\t${property}: ${i}%\n`
+			classes += `\t.${letter}${i}px\n\t\t${property}: ${i}px\n`
+			if (i == 0) table += `\n<tr><td>.${letter}[0-100]pc</td><td>${property}: [0-100]%</td></tr>\n<tr><td>.${letter}[0-100]em</td><td>${property}: [0-100]em</td></tr>`
 		}
 	})
 
-	classes += `\t.vh${i}\n\t\theight: ${i}vh\n\t.vw${i}\n\t\twidth: ${i}vw\n`
-	if (i == 0) table += `\n<tr><td>.vw[0-100]</td><td>width: [0-100]vw</td></tr>\n<tr><td>.vh[0-100]</td><td>height: [0-100]vh</td></tr>\n`
+	classes += `\t.vh${i}\n\t\theight: ${i}vh\n\t.vw${i}\n\t\twidth: ${i}vw\n\t.basis${i}\n\t\tflex-basis: ${i}%\n\t.basis${i}em\n\t\tflex-basis: ${i}em\n`
+	if (i == 0) table += `\n<tr><td>.vw[0-100]</td><td>width: [0-100]vw</td></tr>\n<tr><td>.vh[0-100]</td><td>height: [0-100]vh</td></tr>\n<tr><td>.basis[0-100]</td><td>flex-basis: [0-100]%</td></tr>\n<tr><td>.basis[0-100]em</td><td>flex-basis: [0-100]em</td></tr>\n`
 }
 
 
@@ -382,7 +485,7 @@ fs.writeFile('./_shorthand.sass', file, function(err) {
 
 
 	sass.render({ data: file, indentedSyntax: true }, function(err, result) { 
-		if (err) return console.error('❌ 📜', err.message)
+		if (err) return console.error('❌ 📜', err.message, 'on line: ' + err.line)
 		const str = result.toString()
 		console.log('✅ 📜  successfully compiled _shorthand.sass')
 
