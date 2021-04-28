5d6efb963238f1091b038102dbf07cd6bb76be6c
GSinnott <g@sinnott.cc>
2021-04-28 18:44:55 +0200

    fixes

--- src/App.svelte
+++ src/App.svelte
@@ -11,7 +11,7 @@
 		$root = '<style>'+text+'</style >'
 		hotkeys('ctrl+f,cmd+f', function(event, handler){
 			event.preventDefault() 
-			window.location = '/#/search'
+			window.location.hash = '#/search'
 			setTimeout( e => {
 				document.getElementById('search').focus()
 			}, 10)
@@ -27,36 +27,43 @@
 <main>
 
 	{@html $root}
-	<div class="margin-auto align-self-center w100vw flex justify-content-center h100vh overflow-hidden">
-		<div class="flex basis20em grow justify-content-flex-end overflow-auto">
-			<div class="flex column p1 maxw15em grow justify-content-between">
-				<div class="flex column">
-					<div class="bright f5 pb0">SASSIS</div>
-					{#each Object.entries($routes) as [k,v]}
-						{#if v.$$href != '#/' && v.$$href.indexOf(':') == -1}
-							<div 
-								class="p0-4 bright pointer"
-								class:alert={ k == '/intro'}
-								class:mt1={ k == '/search'}
-								class:info={ k == '/search' || k == '/download'}
-								class:error={ k == '/variables' }
-								on:click={ e => window.location = v.$$href }
-								class:filled={ $active.$$href.indexOf( v.$$href ) != -1 } 
-								class:bright={ $active.$$href.indexOf( v.$$href ) != -1 }>
-								{k.substring(1)}
-							</div>
-						{/if}
-					{/each}
-				</div>
-				<div>
-					<div class="bright">v{infos.package.version} <span class="fade">{date}</span></div>
-				</div>
-			</div>
+	<div class="flex w100vw h100vh overflow-hidden row-center-flex-start">
+		<div class="overflow-hidden flex row-center-flex-start grow">
+			<div class="flex basis20em justify-content-flex-end overflow-auto h100vh">
+				<div class="flex column p1 grow justify-content-between">
+					<div class="flex column">
+						<div class="bright f5 pb0">SASSIS</div>
+						{#each Object.entries($routes) as [k,v]}
+							{#if v.$$href != '#/' && v.$$href.indexOf(':') == -1}
+								<div 
+									class="p0-4 bright pointer"
+									class:alert={ k == '/intro'}
+									class:mt1={ k == '/search'}
+									class:info={ k == '/search' || k == '/download'}
+									class:error={ k == '/variables' }
+									on:click={ e => window.location = v.$$href }
+									class:filled={ $active.$$href.indexOf( v.$$href ) != -1 } 
+									class:bright={ $active.$$href.indexOf( v.$$href ) != -1 }>
+									{k.substring(1)}
+								</div>
+							{/if}
+						{/each}
+					</div>
+					<div>
 
-		</div>
-		<div class="flex basis60em overflow-auto grow">
-			<div class="flex maxw60em grow pt1">
-				<Router />  
+						<div class="bright">
+							v{infos.package.version} 
+							<span class="fade">{date}</span>
+							<a href="https://autr.tv" target="_blank">autr.tv</a>
+						</div>
+					</div>
+				</div>
+
+			</div>
+			<div class="basis40em flex overflow-auto grow h100vh">
+				<div class="flex maxw60em grow pt1">
+					<Router />  
+				</div>
 			</div>
 		</div>
 	</div>
